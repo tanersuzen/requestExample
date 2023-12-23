efc74d74240549de21289499b86ba9934a6fd9e1
Taner Süzen <taner.suzen95@gmail.com>
2023-12-23 20:07:22 +0300

    First Commit

--- main.py
+++ main.py
@@ -13,10 +13,14 @@ print(get_response.json())
 
 #POST -> posting the data
 
-to_do_item= {"userId": 3, "title": "my to do2", "completed": False}
-post_url="https://jsonplaceholder.typicode.com/todos"
-headers = {"Content_Type": "application/json"}
+to_do_item = {"userId" : 3, "title" : "my to do2", "completed" : False}
+post_url = "https://jsonplaceholder.typicode.com/todos"
+header = {"Content_Type" : "application/json"}
 #post_response = requests.post(post_url,json=to_do_item,headers=headers)
-post_response = requests.post(post_url, data= to_do_item, headers=headers)
+post_response = requests.post(url=post_url, data=to_do_item, headers=header)
 #post_response = requests.post(post_url, data= json.dumps(to_do_item), headers=headers)
 print(post_response.json())
+print(post_response.status_code)
+print(json.dumps(to_do_item))
+print(type(json.dumps(to_do_item)))
+print(type(to_do_item))
